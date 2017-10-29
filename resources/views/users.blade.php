e642042816d9308443f91350d3ef1ab5ce9c2049
ramazan <yaru@bk.ru>
2017-10-29 18:30:16 +0300

    add users.blade

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ramazan
- * Date: 29.10.17
- * Time: 16:40
- */
+@extends('layouts.app')
+
+@section('content')
+    <table class="table">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Registered At</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                {{--<td><a href="domains/{{ $item->id }}">{{ $item->name }}</a></td>--}}
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->created_at }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $users->links() }}
+@endsection
