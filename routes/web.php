f9960de220f2d989304b5f2abcd4c9eebd16447c
ramazan <yaru@bk.ru>
2017-10-29 21:42:36 +0300

    add route to "/account"

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/users', 'UsersController@users')->middleware('auth')->name('users');
+
+Route::get('/account', 'AccountController@account')->middleware('auth')->name('account');
