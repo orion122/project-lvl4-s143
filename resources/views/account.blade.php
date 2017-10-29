c3ebf84f0c029e66b760fea199252e28c10f060d
ramazan <yaru@bk.ru>
2017-10-29 23:25:54 +0300

    show account data

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="card col-md-4 my-3 mx-auto">
+            <div class="card-block">
 
+                <h2 class="card-title my-3">Account</h2>
+
+                <div class="form-group row">
+                    <label for="example-text-input" class="col-3 col-form-label">Name:</label>
+                    <div class="col-8">
+                        <input class="form-control" type="text" value="{{ Auth::user()->name }}" id="example-text-input">
+                    </div>
+                </div>
+
+
+                <fieldset disabled>
+                    <div class="form-group row">
+                        <label for="example-text-input" class="col-3 col-form-label">E-mail:</label>
+                        <div class="col-8">
+                            <input class="form-control" type="text" value="{{ Auth::user()->email }}" id="example-text-input">
+                        </div>
+                    </div>
+                </fieldset>
+
+                <div class="my-3">
+                    <button type="submit" class="btn btn-primary">
+                        Change
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
 @endsection
