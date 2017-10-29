81b8dfe9b4d44863d4b2bf0d1e3ac30ece69136d
ramazan <yaru@bk.ru>
2017-10-29 20:11:24 +0300

    edit footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,8 +64,10 @@
         @yield('content')
 
 
-        <footer class="container">
-            <p>&copy; SuperNinja 2017</p>
+        <footer class="footer">
+            <div class="container">
+                <span class="text-muted">&copy; SuperNinja 2017</span>
+            </div>
         </footer>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
