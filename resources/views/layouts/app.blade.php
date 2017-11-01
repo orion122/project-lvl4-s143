171e87a530dc41b23b67f19d4e9d1c886e349584
ramazan <yaru@bk.ru>
2017-11-01 10:23:58 +0300

    change asset to secure_asset for 'sticky-footer.css'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="{!! asset('css/sticky-footer.css') !!}">
+    <link rel="stylesheet" href="{!! secure_asset('css/sticky-footer.css') !!}">
 
     <title>SuperTaskman</title>
 </head>
