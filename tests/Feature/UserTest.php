f5b0c7ddde2c23fec69daf8b31fb1e2129a088e3
ramazan <yaru@bk.ru>
2017-11-16 19:52:41 +0300

    add edit/remove user tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,7 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ramazan
- * Date: 16.11.17
- * Time: 19:42
- */
+
+namespace Tests\Feature;
+
+use \App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function testEditTask()
+    {
+        $user = factory(User::class)->create();
+
+
+        $this->actingAs($user)
+            ->get("/users/$user->id/edit")
+            ->assertStatus(200);
+
+
+        $this->post("/users/$user->id", [
+            'name' => 'new name',
+            '_method' => 'PATCH',
+            '_token' => csrf_token()
+        ])->assertRedirect("/users/$user->id/edit");
+
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => 'new name'
+        ]);
+    }
+
+
+
+    public function testRemoveUser()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user);
+
+
+        $this->post("/users/{$user->id}", [
+            '_method' => 'DELETE',
+            '_token' => csrf_token()
+        ]);
+
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id
+        ]);
+    }
+}
